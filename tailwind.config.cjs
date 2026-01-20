ecb50fcb9fa2cd7402bf55ed9df799adb10cebb6
shadabchow-ui <shadabchow@gmail.com>
2026-01-20 00:25:15 -0500

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './*.{js,ts,jsx,tsx}',          // <-- your App.tsx, Editor.tsx, main.tsx are here
+    './languagetool-master/**/*.{js,ts,jsx,tsx,html}', // optional, safe to include
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
